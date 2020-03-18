758133746226b1cc4fec8002cb52b2bf1990ae06
scottharris86 <scottharrisdev@gmail.com>
2020-03-18 12:32:45 -0400

    added game board tests to the unit testing target

---
+++ TicTacToeTests/GameBoardTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameBoardTests.swift
+//  TicTacToeTests
+//
+//  Created by scott harris on 3/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class GameBoardTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
