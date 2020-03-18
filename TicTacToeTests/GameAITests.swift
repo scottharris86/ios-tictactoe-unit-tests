3186523fc05d4ef44f43e55b463ec7d12a2d2682
scottharris86 <scottharrisdev@gmail.com>
2020-03-18 13:48:34 -0400

    clean up

---
+++ TicTacToeTests/GameAITests.swift
@@ -0,0 +1,30 @@
+//
+//  GameAITests.swift
+//  TicTacToeTests
+//
+//  Created by scott harris on 3/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+@testable import TicTacToe
+
+class GameAITests: XCTestCase {
+
+    func testWinCheckingVertical0() {
+        var board = GameBoard()
+        
+        try! board.place(mark: .x, on: (0,0))
+        try! board.place(mark: .o, on: (1,0))
+        
+        try! board.place(mark: .x, on: (0,1))
+        try! board.place(mark: .o, on: (1,1))
+        
+        try! board.place(mark: .x, on: (0,2))
+        
+        XCTAssertTrue(game(board: board, isWonBy: .x))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+   
+    }
+
+}
